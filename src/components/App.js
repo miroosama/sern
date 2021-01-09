35075af13dfefa79ddcf407a1ea626082e23f1f4
miroosama <o.miro650@gmail.com>
2021-01-08 22:20:44 -0500

    web3 data setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Web3 from 'web3';
 
+import DaiToken from '../abis/DaiToken.json';
+import TimoToken from '../abis/TimoToken.json';
+import TokenFarm from '../abis/TokenFarm.json';
 import Navbar from './Navbar'
 import Web3Wrapper from './Web3Wrapper';
 import './App.css'
@@ -8,11 +11,63 @@ import './App.css'
 export default function App() {
   const [account, setAccount] = useState('0x0');
   const [web3Instance, setWeb3Instance] = useState();
+  const [networkId, setNetworkId] = useState();
+  const [smartContract, setSmartContract] = useState({
+    daiToken: {},
+    timoToken: {},
+    tokenFarm: {}
+  });
+  const [daiTokenBalance, setDaiTokenBalance] = useState();
+  const [timoTokenBalance, setTimoTokenBalance] = useState();
+  const [stakingBalance, setStakingBalance] = useState();
+  // const [loading, setLoading] optional
 
   useEffect(() => {
     const loadWeb3 = async () => {
       const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
       const accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+
+      const daiTokenData = DaiToken.networks[networkId];
+      if (daiTokenData) {
+        const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address);
+        const daiTokenBalance = await daiToken.methods.balanceOf(accounts[0]).call();
+        setSmartContract({
+          ...smartContract,
+          daiToken
+        });
+        setDaiTokenBalance(daiTokenBalance.toString());
+      } else {
+        window.alert('dai token contract not detected');
+      }
+
+      const timoTokenData = TimoToken.networks[networkId];
+      if (timoTokenData) {
+        const timoToken = new web3.eth.Contract(TimoToken.abi, timoTokenData.address);
+        const timoTokenBalance = await timoToken.methods.balanceOf(accounts[0]).call();
+        setSmartContract({
+          ...smartContract,
+          timoToken
+        });
+        setTimoTokenBalance(timoTokenBalance.toString());
+      } else {
+        window.alert('dai token contract not detected');
+      }
+
+      const tokenFarmData = TokenFarm.networks[networkId];
+      if (tokenFarmData) {
+        const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address);
+        const tokenFarmBalance = await tokenFarm.methods.stakingBalance(accounts[0]).call();
+        setSmartContract({
+          ...smartContract,
+          tokenFarm
+        });
+        setStakingBalance(tokenFarmBalance.toString());
+      } else {
+        window.alert('dai token contract not detected');
+      }
+
+      setNetworkId(networkId);
       setAccount(accounts[0]);
       setWeb3Instance(web3);
     };
