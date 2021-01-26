8b56de37479f17e3b99b4c344d4591f88aa72ef5
miroosama <o.miro650@gmail.com>
2021-01-25 23:01:23 -0500

    deployed IF

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 const Wallet = artifacts.require('Wallet');
 const Portfolio = artifacts.require('Portfolio');
-// const InvestmentFund = artifacts.require('InvestmentFund');
+const InvestmentFund = artifacts.require('InvestmentFund');
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(Wallet);
   await deployer.deploy(Portfolio);
-  // await deployer.deploy(InvestmentFund);
+  await deployer.deploy(InvestmentFund, '0x2902Df4Cf4d88308c4cE6563B7b1e7124a2A64b0', 'genesis', 'genesis');
 };
