3503b7f81e02778c14a0ae9e5691107f37b09ab8
miroosama <o.miro650@gmail.com>
2021-01-27 22:46:08 -0500

    compound wallet

---
+++ contracts/CompoundWallet.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.6.0;
+
+interface CEth {
+    function mint() external payable;
+
+    function exchangeRateCurrent() external returns (uint256);
+
+    function supplyRatePerBlock() external returns (uint256);
+
+    function redeem(uint) external returns (uint);
+
+    function redeemUnderlying(uint) external returns (uint);
+}
+
+
+contract CompoundWallet {
+  event WalletLog(string, uint256);
+
+  function supplyEthToCompound(address payable _cEtherContract) public payable returns (bool) {
+    CEth cToken = CEth(cEtherContract);
+
+    uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
+    emit WalletLog("Exchange Rate (scaled up by 1e18): ", exchangeRateMantissa);
+
+    uint256 supplyRateMantissa = cToken.supplyRatePerBlock();
+    emit WalletLog("Supply Rate: (scaled up by 1e18)", supplyRateMantissa);
+
+    cToken.mint.value(msg.value).gas(250000)();
+    return true;
+  }
+
+  function redeemCEth(uint256 amount, bool reedeemType, address cEtherContract) public returns (bool) {
+    CEth cToken = CEth(_cEtherContract);
+
+    uint256 redeemResult cToken.redeemUnderlying(amount);
+
+    emit WalletLog("If this is not 0, there was an error", redeemResult);
+
+    return true;
+  }
+
+  function() external payable {}
+}
